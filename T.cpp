23abc50a728e77baae603913ad1a6da0ad031e11
5+1 <67494254+516wjy@users.noreply.github.com>
2021-05-18 16:54:04 +0800

    Create T.cpp

---
+++ T.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int n, m, a[100005];
+
+int main ()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    int left = 0, right = 1000000005, mid = 0;
+    while (left < right)
+}
