0e829ff87d55004958e31acc1e3ee556615885e1
5+1 <67494254+516wjy@users.noreply.github.com>
2021-05-18 16:57:52 +0800

    Update T.cpp

--- T.cpp
+++ T.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
-int n, m, a[100005];
+int n, m, a[100005], b;
+
+bool check (int x)
+{
+    return x * b
+}
 
 int main ()
 {
@@ -8,7 +13,12 @@ int main ()
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
+        b += a[i];
     }
     int left = 0, right = 1000000005, mid = 0;
     while (left < right)
+    {
+        mid = (left + right) >> 1;
+        if (check(mid))
+    }
 }
